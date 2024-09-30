6630765343f98d3212b1e28c154737d1350954de
Alex Dodge <alexdodge+fcc@gmail.com>
2024-09-29 22:04:19 -0700

    Compelte assignment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer')
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const upload = multer();
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post(
+  "/api/fileanalyse",
+  upload.single('upfile'),
+  (req, res) => {
+    const file = req.file;
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size,
+    });
+  })
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
